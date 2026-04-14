00cf8aaf0a85ba1b6354f21433f0a5b3f1b68e72
Dinocame <100030824@mvla.net>
2026-04-14 13:25:17 -0700

    Edit Code for player attack

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject blastPrefab;
+    public Transform firePoint;
 
-    // Update is called once per frame
+    public float fireDistance = 1.0f; // how far from player
+
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0f;
+
+        // Direction from player to mouse
+        Vector2 direction = (mousePos - transform.position).normalized;
+
+        // Move fire point outward from player
+        Vector2 spawnPosition = (Vector2)transform.position + direction * fireDistance;
+
+        // Calculate rotation
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.Euler(0f, 0f, angle);
+
+        Instantiate(blastPrefab, spawnPosition, rotation);
     }
 }
