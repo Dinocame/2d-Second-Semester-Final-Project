6286d0782cfeb12ca5df2493cd812bb50b244c75
100033145-DividedByZer0 <100033145@mvla.net>
2026-04-14 13:45:07 -0700

    movement

--- Assets/Scripts/PlayerDeath.cs
+++ Assets/Scripts/PlayerDeath.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerDeath : MonoBehaviour
 {
+    public bool isDead = false;
+    
     // Start is called before the first frame update
     void Start()
     {
