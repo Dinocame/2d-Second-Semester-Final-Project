9e93718874814fcfbb4f6362c49e686d7a52eb99
CodyTheBlaze <100033617@mvla.net>
2026-04-20 13:34:54 -0700

    Level 1

--- Assets/Scripts/SpikeDeath.cs
+++ Assets/Scripts/SpikeDeath.cs
@@ -4,7 +4,7 @@ using UnityEngine.SceneManagement;
 
 public class SpikeDeath : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnCollisionEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
