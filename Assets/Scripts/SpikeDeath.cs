ab3edcb0442fd721dc1fbcd1a7cec2895b9e281b
CodyTheBlaze <100033617@mvla.net>
2026-04-21 13:49:08 -0700

    84 files have fun (DividedByZer0 wrote this description :P)

--- Assets/Scripts/SpikeDeath.cs
+++ Assets/Scripts/SpikeDeath.cs
@@ -1,24 +1,29 @@
-
-using UnityEngine;
-using UnityEngine.SceneManagement;
-
-public class SpikeDeath : MonoBehaviour
-{
-    public GameObject isDead;    
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            Die();
-        } 
-    }
-
-    void Die()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-        Debug.Log("Player hit a spike!");
-        
-        isDead = true;
-    }
-   
-}
+//
+// using UnityEngine;
+// using UnityEngine.SceneManagement;
+//
+// public class SpikeDeath : MonoBehaviour
+// {
+//     private GameObject player;
+//
+//     void Start()
+//     {
+//         PlayerDeath temp = player.GetComponent<PlayerDeath>();
+//     }
+//
+//     void OnCollisionEnter2D(Collision2D collision)
+//     {
+//         if (collision.gameObject.CompareTag("Player"))
+//         {
+//             Die();
+//         } 
+//     }
+//
+//     void Die()
+//     {
+//         temp.isDead = true;;
+//         
+//         
+//     }
+//    
+// }
