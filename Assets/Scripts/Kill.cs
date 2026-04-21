2c52df5b46de0e506dd5b9c63ab3dcbd2f28350b
100033145-DividedByZer0 <100033145@mvla.net>
2026-04-21 13:37:32 -0700

    incomplete

---
+++ Assets/Scripts/Kill.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kill : MonoBehaviour
+{
+    public GameObject player;
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Corpse"))
+        {
+            Destroy(collision.gameObject);
+            player.GetComponent<PlayerDeath>().soulPower = soulPower;
+            player.GetComponent<PlayerDeath>().soulPower = soulPower;
+        }
+    }
+}
