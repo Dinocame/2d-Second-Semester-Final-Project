5b72e0ec10c3a7ead926484a52ccc6d784bbf5f5
Dinocame <100030824@mvla.net>
2026-04-27 13:34:55 -0700

    drop corpse

--- Assets/Scripts/EnemyAttack.cs
+++ Assets/Scripts/EnemyAttack.cs
@@ -93,7 +93,7 @@ public class EnemyAttack : MonoBehaviour
     {
         movement.LockMovement(0.5f);
 
-        float direction = movement.GetDirection();
+        float direction = -movement.GetDirection();
 
         Collider2D col = GetComponent<Collider2D>();
         float enemyHalfWidth = col.bounds.extents.x;
