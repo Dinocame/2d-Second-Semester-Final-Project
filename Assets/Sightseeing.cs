6736b2504583c1c332d4d20b1495a36798cfb1ee
CodyTheBlaze <100033617@mvla.net>
2026-05-07 13:40:57 -0700

    Level 2 improvment + Triangular stuff

---
+++ Assets/Sightseeing.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Sightseeing : MonoBehaviour
+{
+
+    public float zoomOutCamSize = 10f;
+    public float zoomTime = 1f;
+    private float originalCamSize;
+    private CinemachineVirtualCamera  virtualCamera;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        virtualCamera = FindObjectOfType<CinemachineVirtualCamera>();  
+        originalCamSize = virtualCamera.m_Lens.OrthographicSize;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            StopAllCoroutines();
+            StartCoroutine(SmoothZoom(virtualCamera.m_Lens.OrthographicSize, zoomOutCamSize, zoomTime));
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        StopAllCoroutines();
+        StartCoroutine(SmoothZoom(virtualCamera.m_Lens.OrthographicSize, originalCamSize, zoomTime));
+    }
+
+    IEnumerator SmoothZoom(float start, float end, float zoomTime)
+    {
+        for (float t = 0.0f; t < zoomTime; t += Time.deltaTime)
+        {
+            virtualCamera.m_Lens.OrthographicSize = Mathf.Lerp(start, end, t/zoomTime);
+            yield return null;
+        }
+    }
+}
