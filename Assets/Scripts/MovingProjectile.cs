5c6e6383e1c146d17413beba5d1a07bbbb96102b
Dinocame <100030824@mvla.net>
2026-05-14 14:02:34 -0700

    player moves

--- Assets/Scripts/MovingProjectile.cs
+++ Assets/Scripts/MovingProjectile.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 public class MovingProjectile : MonoBehaviour
 {
     public float speed = 10f;
-    public float lifetime = 3f;
+    public float lifetime = 1.5f;
 
     private Vector2 moveDirection;
+
+    private SpriteRenderer sr;
+
+    private float timer = 0f;
+
+    public float fadeInPercent = 0.1f;
 
     public void SetDirection(Vector2 direction)
     {
@@ -19,11 +25,39 @@ public class MovingProjectile : MonoBehaviour
 
     void Start()
     {
-        Destroy(gameObject, lifetime);
+        sr = GetComponent<SpriteRenderer>();
+
+        Color color = sr.color;
+        color.a = 0.1f;
+        sr.color = color;
     }
 
     void Update()
     {
+        timer += Time.deltaTime;
+
         transform.position += (Vector3)(moveDirection * speed * Time.deltaTime);
+
+        float lifePercent = timer / lifetime;
+
+        float alpha;
+
+        if (lifePercent < fadeInPercent)
+        {
+            alpha = Mathf.Lerp(0.1f, 1f, lifePercent / fadeInPercent);
+        }
+        else
+        {
+            alpha = Mathf.Lerp(1f, 0f,(lifePercent - fadeInPercent) / (1f - fadeInPercent));
+        }
+
+        Color color = sr.color;
+        color.a = alpha;
+        sr.color = color;
+
+        if (timer >= lifetime)
+        {
+            Destroy(gameObject);
+        }
     }
 }
